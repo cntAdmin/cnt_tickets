1c6222695f4d39d755687c7f704f52479905cccd
Matias <matias@costanetworks.es>
2023-03-17 17:55:53 +0100

    mensaje de bienvenida

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,25 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\ModelHasRoles;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Models\ModelHasRoles;
 
 class TestController extends Controller
 {
     public function my_custom_tests()
     {
-        $users = User::all();
-        // dd($users);
+        // $users = User::all();
+        // // dd($users);
         
-        foreach ($users as $user){
-            // dd($user->id);
+        // foreach ($users as $user){
+        //     // dd($user->id);
 
-            ModelHasRoles::create([
-                'role_id' => 3,
-                'model_type' => 'App\Models\User',
-                'model_id' => $user->id
-            ]);
-        }
+        //     ModelHasRoles::create([
+        //         'role_id' => 3,
+        //         'model_type' => 'App\Models\User',
+        //         'model_id' => $user->id
+        //     ]);
+        // }
+        dd('hola cotilla');
     }
 }
